b34cf05323d5fcd7682af77c5a18981cded15e1b
skrepkoed <faust898@mail.ru>
2021-04-24 20:01:54 +0500

    Реализована валидация для ссылок

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -14,5 +14,7 @@ RSpec.describe Answer, type: :model do
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :url}
+    it {should allow_value('https://www.google.com/').for(:url)}
+    it {should_not allow_value('h/www.\googlecom/').for(:url)}
   end
 end
