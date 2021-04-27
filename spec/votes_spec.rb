7ffa3379912f5b6e6da4101af0ad8c72587ad68e
skrepkoed <faust898@mail.ru>
2021-04-27 21:51:26 +0500

    Реализована история Пользователь не может голосовать за свой вопрос

--- spec/votes_spec.rb
+++ spec/votes_spec.rb
@@ -19,6 +19,37 @@ feature 'User can vote for answer or question',
         click_on 'Vote pro'
         expect(page).to have_content 'Rating: 1'
       end
+    end
+
+    describe 'User can`t vote for his own question' do
+      given!(:question) { create(:question) }
+      given!(:user) { question.author }
+
+      background do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      scenario 'User vote for question and counter increments' do
+        expect(page).to have_content 'Rating: 0'
+        expect(page).to_not have_content 'Vote pro'
+      end
     end    
+  end
+
+  describe 'User isn`t authenticated' do
+    describe 'User can`t vote for question' do
+      given(:question) { create(:question) }
+      given(:user) { create(:user) }
+      
+      background do
+        visit question_path(question)
+      end
+
+      scenario 'User vote for question and counter increments' do
+        expect(page).to have_content 'Rating: 0'
+        expect(page).to_not have_content 'Vote pro'
+      end
+    end  
   end
 end
