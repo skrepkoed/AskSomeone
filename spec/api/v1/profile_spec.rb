e41ba0cfe1daaf870bafaf2fa79f96d56e6d23cb
skrepkoed <faust898@mail.ru>
2021-05-16 12:13:50 +0500

    Реализовано API для профиля пользователя

--- spec/api/v1/profile_spec.rb
+++ spec/api/v1/profile_spec.rb
@@ -11,6 +11,7 @@ describe 'Profile Api', type: :request do
     context 'authorized' do
       let(:me){ create(:user) }
       let(:access_token){ create(:access_token, resource_owner_id:me.id) }
+      let(:user_reponse){ json['user'] }
       before { get '/api/v1/profiles/me', params: {access_token: access_token.token}, headers: headers }
       it 'returns 200 status' do
         expect(response).to be_successful
@@ -18,13 +19,13 @@ describe 'Profile Api', type: :request do
       
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(user_reponse[attr]).to eq me.send(attr).as_json
         end
       end
 
       it 'does not return all public fields' do
         %w[password encrypted_password].each do |attr|
-          expect(json).to_not have_key(attr)
+          expect(user_reponse).to_not have_key(attr)
         end
       end
     end
