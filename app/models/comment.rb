6f828b723330cb49c923860dc2fbeb47d7116ff6
skrepkoed <faust898@mail.ru>
2021-06-05 17:29:19 +0500

    Добавлено кэширование фрагментов

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
-  # validates :body, presence: true
+  validates :body, presence: true
 
   def question
     if commentable_type == 'Question'
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ApplicationRecord
-  belongs_to :linkable, polymorphic: true
+  belongs_to :linkable, polymorphic: true, touch: true
 
   validates :name, :url, presence: true
   validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp }
